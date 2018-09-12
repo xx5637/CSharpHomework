2130e9f73fedbd44c153223ec3f52f20d4d82483
xx5637 <43162420+xx5637@users.noreply.github.com>
2018-09-12 12:03:46 +0800

    Add files via upload

---
+++ homework1/question4/question4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question4
+{
+    class question4
+    {
+        static void Main(string[] args)
+        {
+            double result;
+            Console.WriteLine("Please input the first number:");
+            string Str1 = Console.ReadLine();
+            double Number1 = double.Parse(Str1);
+            Console.WriteLine("Please input the second number:");
+            string Str2 = Console.ReadLine();
+            double Number2 = double.Parse(Str2);
+            result = Number1 * Number2;
+            Console.WriteLine("The result is:" + result);
+        }
+    }
+}
---
+++ homework1/question5/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace question5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string txt1 = textBox1.Text;
+            string txt2 = textBox2.Text;
+            int num1 = 0;
+            int num2 = 0;
+            int result = 0;
+            num1 = int.Parse(txt1);
+            num2 = int.Parse(txt2);
+            result = num1 * num2;
+            label1.Text = "两整数乘积为" + result;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
