31177417f2f45326ccc88357cae56bd7a8265c6c
xx5637 <43162420+xx5637@users.noreply.github.com>
2018-10-17 18:09:53 +0800

    Add files via upload

---
+++ homework5/program1/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class orderprogram
+    {
+        public class OrderDetails : Object
+        {
+            public String num;
+            public String name;
+            public String customer;
+            public int price;
+            public override string ToString()
+            {
+                return "The number is:" + num + "\t" + "The product name is：" + name + "\t" + "The customer is：" + customer + "\t" + "the price of the product is：" + price;
+            }
+        }
+        public class OrderService
+        {
+            public List<OrderDetails> testList = new List<OrderDetails>();
+            public void creatorder()
+            {
+                OrderDetails order = new OrderDetails();
+                Console.WriteLine("input the number of the product：");
+                order.num = Console.ReadLine();
+                Console.WriteLine("input the name of the product：");
+                order.name = Console.ReadLine();
+                Console.WriteLine("input the name of the customer：");
+                order.customer = Console.ReadLine();
+                Console.WriteLine("input the price of the product：");
+                order.price = Convert.ToInt32(Console.ReadLine());
+                testList.Add(order);
+            }
+            public void ShowList()
+            {
+                foreach (OrderDetails order in testList)
+                {
+                    Console.WriteLine(order.ToString());
+                }
+            }
+            public void Deletelist()
+            {
+                try
+                {
+                    Console.WriteLine(" input the number of which one you want to delete：");
+                    string num = Console.ReadLine();
+                    int n = Convert.ToInt32(num);
+                    testList.RemoveAt(n - 1);
+                }
+                catch
+                {
+                    Console.WriteLine("error!please retry!");
+                    Deletelist();
+                }
+            }
+            public void ChangeList()
+            {
+                OrderDetails order = new OrderDetails();
+                Console.WriteLine("input the number of the product：");
+                order.num = Console.ReadLine();
+                foreach (OrderDetails o in testList)
+                {
+                    if (o.num == order.num)
+                    {
+                        Console.WriteLine("input new product name：");
+                        o.name = Console.ReadLine();
+                        Console.WriteLine("input new customer：");
+                        o.customer = Console.ReadLine();
+                        Console.WriteLine("input new price：");
+                        o.price = Convert.ToInt32(Console.ReadLine());
+                    }
+                };
+            }
+            public void Searchlist()
+            {
+                OrderDetails order = new OrderDetails();
+                Console.WriteLine("choose 1.search for number");
+                Console.WriteLine("       2.search for product");
+                Console.WriteLine("       3.search for customer");
+                Console.WriteLine("       4.search for the product whose price is over 10000");
+                string option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        Console.WriteLine("input the number：");
+                        order.num = Console.ReadLine();
+                        var i = from n in testList where n.num == order.num select n;
+                        foreach (OrderDetails o in i)
+                        {
+                            Console.WriteLine(o.ToString());
+                        };
+                        break;
+                    case "2":
+                        Console.WriteLine("input the name of the product：");
+                        order.name = Console.ReadLine();
+                        var j = from n in testList where n.name == order.name select n;
+                        foreach (OrderDetails o in j)
+                        {
+                            Console.WriteLine(o.ToString());
+                        }
+                        break;
+                    case "3":
+                        Console.WriteLine("input the name of the customer：");
+                        order.customer = Console.ReadLine();
+                        foreach (OrderDetails o in testList)
+                        {
+                            if (order.name == o.name)
+                            {
+                                Console.WriteLine("number: " + o.num + " product " + o.name + " customer: " + o.customer);
+                            }
+                        };
+                        break;
+                    case "4":
+                        var k = from n in testList where n.price > 10000 select n;
+                        foreach (OrderDetails o in k)
+                        {
+                            Console.WriteLine(o.ToString());
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("please choose your operation from 1-4");
+                        break;
+                }
+            }
+        }
+        public void servicestart(OrderService neworder)
+        {
+            bool a = true;
+            while (a == true)
+            {
+                Console.WriteLine("choose in the following:" + "\n" + "       1.addorder" + "\n" + "       2.deleteorder" + "\n" + "       3.changeorder" + "\n" + "       4.searchorder" + "\n" + "       5.look at all orders" + "\n" + "       6.Finish");
+                string option = Console.ReadLine();
+                switch (option)
+                {
+                    case "1":
+                        neworder.creatorder();
+                        break;
+                    case "2":
+                        neworder.Deletelist();
+                        break;
+                    case "3":
+                        neworder.ChangeList();
+                        break;
+                    case "4":
+                        neworder.Searchlist();
+                        break;
+                    case "5":
+                        neworder.ShowList();
+                        break;
+                    case "6": a = false; break;
+                    default:
+                        Console.WriteLine("error!please make a choice from 1-6");
+                        break;
+                }
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                orderprogram program1 = new orderprogram();
+                OrderService order = new OrderService();
+                program1.servicestart(order);
+            }
+        }
+    }
+}
---
+++ homework5/program2/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+        static int leftangle = 30, rightangle = 20;
+        static double per1 = 0.6;
+        static double per2 = 0.6;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try { leftangle = Convert.ToInt32(this.textBox1.Text); }
+            catch { };
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            try { rightangle = Convert.ToInt32(this.textBox2.Text); }
+            catch { };
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            try { per1 = Convert.ToDouble(this.textBox3.Text); }
+            catch { };
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            try { per2 = Convert.ToDouble(this.textBox4.Text); }
+            catch { };
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        private Graphics graphics;
+       
+        
+
+        void drawCayleyTree(int n, double x0, double y0, double leng, double th)
+        {
+            if (n == 0) return;
+            double x1 = x0 + leng * Math.Cos(th);
+            double x2 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+            double y2 = y0 + 0.5 * leng * Math.Sin(th);
+            drawLine(x0, y0, x1, y1);
+            double th1 = leftangle * Math.PI / 180;
+            double th2 = rightangle * Math.PI / 180;
+            drawCayleyTree(n - 1, x2, y2, per1 * leng, th + th1);
+            drawCayleyTree(n - 1, x1, y1, per2 * leng, th - th2);
+        }
+        void drawLine(double x0, double y0, double x1, double y1)
+        {
+            graphics.DrawLine(
+                Pens.Blue, (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (graphics == null) graphics = this.CreateGraphics();
+            drawCayleyTree(10, 400, 310, 100, -Math.PI / 2);
+        }     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+   
+    }
+}
